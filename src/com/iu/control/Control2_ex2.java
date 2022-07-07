4921f9365a300d85f6dae7d777826d5c75f4f670
hyeongminyoo <dbgudals2004@gmail.com>
2022-07-07 09:05:06 +0900

    fin

--- src/com/iu/control/Control2_ex2.java
+++ src/com/iu/control/Control2_ex2.java
@@ -14,11 +14,20 @@ public class Control2_ex2 {
 		System.out.println("알파벳을 입력");
 		char ch = sc.next().charAt(0);
 		
+		
+		if(ch=='z') {
+			ch = (char)(ch - 26);
+		}
+		if(ch=='Z') {
+			ch = (char)(ch - 26);
+		}
+		
 		ch = (char)(ch+num);
+		
+		
 		System.out.println("ch:"+ch);
+
 		
-		
-	
 	}
 
 }
